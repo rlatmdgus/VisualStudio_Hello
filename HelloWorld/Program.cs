cd634bc93f011312cc8efce1831aa6d2b393f26c
rlatmdgus <55964371+rlatmdgus@users.noreply.github.com>
2019-11-07 09:21:03 +0900

    Update Program.cs
    github commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,6 +14,7 @@ namespace HelloWorld
             Console.WriteLine("hotfix : Hello World");
             Console.WriteLine("hotfix: 2nd Hello World");
             Console.WriteLine("master: 2nd Hello World");
+            Console.WriteLine("github : Hello World");
         }
     }
 }
